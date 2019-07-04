264455504083071266e3c61ad63c574c88e97979
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-04 19:50:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,28 @@
-school=school.new("Bayside High School")
-school.roster
-school.add_student("Zach Morris", 9)
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
